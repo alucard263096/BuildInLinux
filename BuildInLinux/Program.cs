ff731756a211d2bd6a078fc90bc13769a5c1ab56
scai <alucard263096@126.com>
2022-05-27 09:05:35 +0800

    aaa

---
+++ BuildInLinux/Program.cs
@@ -0,0 +1,19 @@
+﻿using CommonLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildInLinux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            c.count(1, 2);
+            Console.Read();
+        }
+    }
+}
